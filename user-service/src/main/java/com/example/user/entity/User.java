f11854c94cdd8f7dfb861f9db34af4a756b4c37c
chaitanya <chaitanya.thulasaku@spectraforce.com>
2023-11-27 13:36:28 +0530

    microservices - java

---
+++ user-service/src/main/java/com/example/user/entity/User.java
@@ -0,0 +1,22 @@
+package com.example.user.entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "usersD")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long userId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private Long departmentId;
+}
